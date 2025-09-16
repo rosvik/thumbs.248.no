d8e316b0bd66672b202fa96a8ff640c73e091d08
Johannes Røsvik <j.rosvik@gmail.com>
2025-09-16 18:17:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# YouTube Thumbnail proxy
+
+This is a simple proxy for YouTube thumbnails. It will fetch the best quality thumbnail for a given YouTube video.
+
+## Known URL formats
+
+`https://[DOMAIN].ytimg.com/vi[_webp]/[VIDEO_ID]/[QUALITY].[FORMAT]`
+
+*Domains*
+- https://i.ytimg.com
+- https://i2.ytimg.com
+- https://i3.ytimg.com
+- https://i4.ytimg.com
+
+*16:9*
+- https://i.ytimg.com/vi/aGb3AlQrN9E/maxresdefault.jpg
+- https://i.ytimg.com/vi/aGb3AlQrN9E/hq720.jpg
+- https://i.ytimg.com/vi/aGb3AlQrN9E/mqdefault.jpg
+
+*4:3*
+- https://i.ytimg.com/vi/aGb3AlQrN9E/sddefault.jpg
+- https://i.ytimg.com/vi/aGb3AlQrN9E/hqdefault.jpg
+- https://i.ytimg.com/vi/aGb3AlQrN9E/0.jpg
+- https://i.ytimg.com/vi/aGb3AlQrN9E/default.jpg
+
+*Formats*
+- https://i.ytimg.com/vi/aGb3AlQrN9E/maxresdefault.jpg
+- https://i.ytimg.com/vi_webp/aGb3AlQrN9E/maxresdefault.webp
+
+
+## Test cases
+
+- maxresdefault `aGb3AlQrN9E`
+- hqdefault `jNQXAC9IVRw`
---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use axum::{
+    Router, body::Body, extract::Path, http::Response, response::IntoResponse, routing::get,
+};
+use regex::Regex;
+use tokio::{fs::File, io::AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/{video_id}", get(get_thumbnail));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_thumbnail(Path(video_id): Path<String>) -> impl IntoResponse {
+    if !validate_video_id(&video_id) {
+        return fallback_response(400);
+    }
+
+    // If the image is already cached, return it
+    let cached_data = fetch_from_cache(&video_id).await;
+    if let Some(cached_data) = cached_data {
+        println!("Returning cached thumbnail for {video_id}");
+        return webp_response(cached_data);
+    }
+
+    let url = format!("https://i.ytimg.com/vi_webp/{video_id}/maxresdefault.webp");
+    let response = match reqwest::get(&url).await {
+        Ok(response) => response,
+        Err(e) => {
+            println!("Error fetching thumbnail: {url}: {e}");
+            return fallback_response(500);
+        }
+    };
+
+    if response.status() != 200 {
+        println!("Error fetching thumbnail: {url}: {}", response.status());
+        return fallback_response(response.status().into());
+    }
+
+    let body = response.bytes().await.unwrap();
+
+    // Save the image to a file
+    let path = format!("thumbnails/{}.webp", video_id);
+    let file_data = body.clone();
+    tokio::spawn(async move {
+        let file = File::create(path).await;
+        if let Ok(mut file) = file {
+            let _ = file.write_all(&file_data).await;
+        }
+    });
+
+    println!("Fetched new thumbnail for {}", video_id);
+    webp_response(body.to_vec())
+}
+
+async fn fetch_from_cache(video_id: &String) -> Option<Vec<u8>> {
+    let path = format!("thumbnails/{video_id}.webp");
+    if std::fs::metadata(&path).is_ok() {
+        let data = match std::fs::read(&path) {
+            Ok(data) => data,
+            Err(e) => {
+                println!("Error reading cached thumbnail: {}: {}", path, e);
+                return None;
+            }
+        };
+        return Some(data);
+    }
+    None
+}
+
+fn webp_response(data: Vec<u8>) -> Response<Body> {
+    Response::builder()
+        .header("Content-Type", "image/webp")
+        .body(Body::from(data))
+        .unwrap()
+}
+
+fn fallback_response(status: u16) -> Response<Body> {
+    let fallback_image = include_bytes!("../fallback.webp");
+    Response::builder()
+        .status(status)
+        .header("Content-Type", "image/webp")
+        .body(Body::from(fallback_image.to_vec()))
+        .unwrap()
+}
+
+/// Validate the video ID is a valid YouTube video ID
+///
+/// Source: https://wiki.archiveteam.org/index.php/YouTube/Technical_details
+fn validate_video_id(video_id: &str) -> bool {
+    let re = Regex::new(r"^[A-Za-z0-9_-]{10}[AEIMQUYcgkosw048]$").unwrap();
+    re.is_match(video_id)
+}
