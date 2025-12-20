d3a33760602355222ce494352b4b7fc91d36027b
Johannes Røsvik <j.rosvik@gmail.com>
2025-12-20 13:41:47 +0100

    feat: add path style setting for local development

--- src/storage.rs
+++ src/storage.rs
@@ -16,7 +16,12 @@ pub async fn get_connection() -> Box<s3::Bucket> {
         security_token: None,
         session_token: None,
     };
-    s3::Bucket::new(&std::env::var("S3_BUCKET").unwrap(), region(), credentials).unwrap()
+    let mut bucket =
+        s3::Bucket::new(&std::env::var("S3_BUCKET").unwrap(), region(), credentials).unwrap();
+    if std::env::var("S3_PATH_STYLE").unwrap_or_default() == "true" {
+        bucket.set_path_style();
+    }
+    bucket
 }
 
 pub async fn put_object(
@@ -42,29 +47,15 @@ pub async fn get_object(
 //     #[tokio::test]
 //     async fn test_s3_connection() {
 //         dotenv::dotenv().ok();
-
-//         let access_key = std::env::var("S3_ACCESS_KEY").unwrap();
-//         let secret_key = std::env::var("S3_SECRET_KEY").unwrap();
-//         let region = std::env::var("S3_REGION").unwrap();
-//         let endpoint = std::env::var("S3_ENDPOINT").unwrap();
-//         let bucket = std::env::var("S3_BUCKET").unwrap();
-
-//         let credentials = Credentials {
-//             access_key: Some(access_key),
-//             secret_key: Some(secret_key),
-//             expiration: None,
-//             security_token: None,
-//             session_token: None,
-//         };
-//         let region = s3::Region::Custom { region, endpoint };
-
-//         let s3 = s3::Bucket::new(&bucket, region, credentials).unwrap();
-
 //         let content = "Hello, world!".as_bytes();
-//         s3.put_object("/test.txt", content).await.unwrap();
-//         let result = s3.get_object("/test.txt").await.unwrap();
+//         let key = "test.txt";
+
+//         let bucket = get_connection().await;
+//         bucket.put_object(key, content).await.unwrap();
+
+//         let result = bucket.get_object(key).await.unwrap();
 //         assert_eq!(result.bytes(), content);
 
-//         s3.delete_object("/test.txt").await.unwrap();
+//         bucket.delete_object(key).await.unwrap();
 //     }
 // }
