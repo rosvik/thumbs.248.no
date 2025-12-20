dc54fb5d00ac6a10a719f2e0e924427a7999c563
Johannes Røsvik <j.rosvik@gmail.com>
2025-12-20 13:12:25 +0100

    refactor: remove FileEstension and Slug traits

--- src/quality.rs
+++ src/quality.rs
@@ -9,40 +9,26 @@ pub enum Quality {
     WebpHq,
     JpgHq,
 }
+
+impl Quality {
+    pub fn file_extension(&self) -> &str {
+        match self {
+            Quality::WebpMaxres | Quality::WebpSd | Quality::WebpHq => "webp",
+            Quality::JpgMaxres | Quality::JpgSd | Quality::JpgHq => "jpg",
+        }
+    }
+
+    pub fn slug(&self) -> &str {
+        match self {
+            Quality::WebpMaxres | Quality::JpgMaxres => "maxresdefault",
+            Quality::WebpSd | Quality::JpgSd => "sddefault",
+            Quality::WebpHq | Quality::JpgHq => "hqdefault",
+        }
+    }
+}
+
 impl fmt::Display for Quality {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} {}", self.slug(), self.file_extension())
-    }
-}
-
-pub trait FileExtension {
-    fn file_extension(&self) -> &str;
-}
-impl FileExtension for Quality {
-    fn file_extension(&self) -> &str {
-        match self {
-            Quality::WebpMaxres => "webp",
-            Quality::JpgMaxres => "jpg",
-            Quality::WebpSd => "webp",
-            Quality::JpgSd => "jpg",
-            Quality::WebpHq => "webp",
-            Quality::JpgHq => "jpg",
-        }
-    }
-}
-
-pub trait Slug {
-    fn slug(&self) -> &str;
-}
-impl Slug for Quality {
-    fn slug(&self) -> &str {
-        match self {
-            Quality::WebpMaxres => "maxresdefault",
-            Quality::JpgMaxres => "maxresdefault",
-            Quality::WebpSd => "sddefault",
-            Quality::JpgSd => "sddefault",
-            Quality::WebpHq => "hqdefault",
-            Quality::JpgHq => "hqdefault",
-        }
     }
 }
